26a03c697186486e591aac8a8babea554ef0862c
zzangyoon0710@gmail.com <zzangyoon710@gmail.com>
2023-03-20 16:15:21 +0900

    최초커밋

---
+++ src/main/java/com/test/testArtifact/TestArtifactApplication.java
@@ -0,0 +1,13 @@
+package com.test.testArtifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestArtifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/testArtifact/TestArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.testArtifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
